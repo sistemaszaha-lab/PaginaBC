317d37c973dbb4375110da53743e37f7b2abe57a
sistemaszaha-lab <sistemaszaha@gmail.com>
2026-08-24 16:52:38 -0600

    nuevo

--- solicitudes_app/urls.py
+++ solicitudes_app/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from . import trash_views
 
 urlpatterns = [
@@ -47,7 +48,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
